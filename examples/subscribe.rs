9e88fc4f2aa1c35746f7bfe84a32921d285a2ed4
HoshinoTented <limbolrain@gmail.com>
2020-05-14 20:50:33 +0800

    [ Example ] Add subscribe_filter

--- examples/subscribe.rs
+++ examples/subscribe.rs
@@ -2,9 +2,11 @@ mod connect;
 
 use connect::connect;
 use std::sync::{Arc, Mutex};
-use std::sync::mpsc::{Sender, Receiver, channel};
+use std::sync::mpsc::{Sender, Receiver, channel, IntoIter};
 use mirai::message::EventPacket;
+use mirai::message::event::MessageEvent;
 use mirai::session::Session;
+use std::iter::Filter;
 
 type Bus = Arc<Mutex<Vec<Sender<EventPacket>>>>;
 
@@ -48,14 +50,30 @@ fn subscribe(bus: Bus) -> Receiver<EventPacket> {
     rc
 }
 
+type SubscribeFilter = Filter<IntoIter<EventPacket>, fn(&EventPacket) -> bool>;
+
+fn subscribe_filter(bus: Bus, predicate: fn(&EventPacket) -> bool) -> SubscribeFilter {
+    subscribe(bus).into_iter().filter(predicate)
+}
+
+fn subscribe_group_message(bus: Bus) -> SubscribeFilter {
+    subscribe_filter(bus, |e| {
+        if let EventPacket::MessageEvent(MessageEvent::GroupMessage { .. }) = e {
+            true
+        } else {
+            false
+        }
+    })
+}
+
 #[tokio::main]
 async fn main() {
     let session = Arc::new(connect().await);
     let bus = init_subscribe(session);
 
-    let rc = subscribe(bus);
+    let rc = subscribe_group_message(bus);
 
-    for packet in rc.iter() {
+    for packet in rc {
         println!("{:?}", packet);
     }
 }
