17a81e1aaebdea1cbc3f2d0b04318c9b97199512
HoshinoTented <limbolrain@gmail.com>
2020-09-16 07:46:46 +0800

    Add `bound` field into `Session`

--- mirai_rs/src/session.rs
+++ mirai_rs/src/session.rs
@@ -49,17 +49,181 @@ use crate::{Code, Target};
 
 #[derive(Clone, Debug)]
 pub struct MiraiConnection {
-    pub base_url: String,
+    pub(crate) base_url: String,
     pub(crate) client: Client,
+}
+
+impl MiraiConnection {
+    /// Constructing a connection with a server address and a mirai client instance.
+    pub fn new(base_url: &str, client: Client) -> MiraiConnection {
+        MiraiConnection {
+            base_url: base_url.to_string(),
+            client,
+        }
+    }
+
+    pub fn base_url(&self) -> &str {
+        &self.base_url
+    }
+
+    /// Connecting the base url from this connection and the given string.
+    /// Note that this function only simply connects two string, so you must ensure the given string starts with the '/' separator.
+    pub fn url(&self, path: &str) -> String {
+        self.base_url.clone() + path
+    }
+
+    /// send a GET request in order to get the information of the mirai server.
+    pub async fn about(&self) -> HttpResult<AboutResponse> {
+        let resp: AboutResponse = self.client.get(&self.url("/about"))
+            .send().await?
+            .json().await?;
+
+        Ok(resp)
+    }
+
+    pub async fn auth(&self, auth_key: &str) -> HttpResult<Session> {
+        #[derive(Serialize)]
+        struct Request {
+            #[serde(rename = "authKey")]
+            auth_key: String
+        }
+
+        #[derive(Deserialize)]
+        struct Response {
+            code: Code,
+            session: Option<String>,
+        }
+
+        let req = Request {
+            auth_key: auth_key.to_string()
+        };
+
+        let result: Response = self.client.post(&self.url("/auth"))
+            .json(&req).send().await?
+            .json().await?;
+
+        assert(result.code, "Auth")?;
+
+        Ok(Session {
+            connection: self.clone(),
+            key: result.session.unwrap(),
+            bound: None,
+        })
+    }
+
+    #[deprecated()]
+    pub async fn run_command(&self, auth_key: &str, command: &str, args: &[&str]) -> HttpResult<String> {
+        #[serde(rename_all = "camelCase")]
+        #[derive(Serialize)]
+        struct Request<'s> {
+            auth_key: &'s str,
+            name: &'s str,
+            args: &'s [&'s str],
+        }
+
+        let req = Request {
+            auth_key,
+            name: command,
+            args,
+        };
+
+        let text = self.client.post(&self.url("/command/send"))
+            .json(&req).send().await?
+            .text().await?;
+
+        Ok(text)
+    }
 }
 
 /// # Session
 ///
-/// Session is a structure that contains a connection with the server which is authorized, and a Auth Key which received from server.
+/// Session contains a connection with the server which is authorized, an Auth Key which received from server, and a bound bot.
 #[derive(Debug)]
 pub struct Session {
-    pub connection: MiraiConnection,
-    pub key: String,
+    pub(crate) connection: MiraiConnection,
+    pub(crate) key: String,
+    pub(crate) bound: Option<Target>,
+}
+
+impl Session {
+    /// Return the session key of this session
+    /// **WARNING: Session Key SHOULD BE Secret.**
+    pub fn key(&self) -> &str {
+        &self.key
+    }
+
+    /// Connecting the base url from this connection and the given string.
+    /// Note that this function only simply connects two string, so you must ensure the given string starts with the '/' separator.
+    pub fn url(&self, path: &str) -> String {
+        self.connection.url(path)
+    }
+
+    /// Return the client of this session
+    pub fn client(&self) -> &Client {
+        &self.connection.client
+    }
+
+    /// Binding the session with the given QQ ID.
+    /// Note that one session can only bind with one QQ ID.
+    pub async fn verify(&mut self, qq: Target) -> HttpResult<()> {
+        #[derive(Serialize)]
+        struct Request {
+            #[serde(rename = "sessionKey")]
+            session_key: String,
+            qq: Target,
+        }
+
+        let req = Request {
+            session_key: self.key.clone(),
+            qq,
+        };
+
+        let result: CommonResponse = self.client().post(&self.url("/verify"))
+            .json(&req).send().await?
+            .json().await?;
+
+        assert(result.code, "Verify")?;
+
+        self.bound = Some(qq);
+
+        Ok(())
+    }
+
+    /// Release a bot which current session bound before.
+    pub async fn release(&self) -> HttpResult<()> {
+        unsafe {
+            self.release_unchecked(self.bound.unwrap_or(0)).await
+        }
+    }
+
+    pub async unsafe fn release_unchecked(&self, qq: Target) -> HttpResult<()> {
+        #[derive(Serialize)]
+        struct Request {
+            #[serde(rename = "sessionKey")]
+            session_key: String,
+            qq: Target,
+        }
+
+        let req = Request {
+            session_key: self.key.clone(),
+            qq,
+        };
+
+        let resp: CommonResponse = self.client().post(&self.url("/release"))
+            .json(&req).send().await?
+            .json().await?;
+
+        assert(resp.code, "Release")
+    }
+}
+
+/// release without unwrap (because current session might not bind any bot before)
+impl Drop for Session {
+    fn drop(&mut self) {
+        tokio::runtime::Runtime::new().unwrap().block_on(async {
+            self.release().await
+        });
+    }
 }
 
 /// # CommonResponse
@@ -120,137 +284,5 @@ impl AboutData {
 
     pub fn version(&self) -> &String {
         &self.version
-    }
-}
-
-impl MiraiConnection {
-    /// Constructing a connection with a server address and a mirai client instance.
-    pub fn new(base_url: &str, client: Client) -> MiraiConnection {
-        MiraiConnection {
-            base_url: base_url.to_string(),
-            client,
-        }
-    }
-
-    /// Connecting the base url from this connection and the given string.
-    /// Note that this function only simply connects two string, so you must ensure the given string starts with the '/' separator.
-    pub fn url(&self, path: &str) -> String {
-        self.base_url.clone() + path
-    }
-
-    /// send a GET request in order to get the information of the mirai server.
-    pub async fn about(&self) -> HttpResult<AboutResponse> {
-        let resp: AboutResponse = self.client.get(&self.url("/about"))
-            .send().await?
-            .json().await?;
-
-        Ok(resp)
-    }
-
-    pub async fn auth(&self, auth_key: &str) -> HttpResult<Session> {
-        #[derive(Serialize)]
-        struct Request {
-            #[serde(rename = "authKey")]
-            auth_key: String
-        }
-
-        #[derive(Deserialize)]
-        struct Response {
-            code: Code,
-            session: Option<String>,
-        }
-
-        let req = Request {
-            auth_key: auth_key.to_string()
-        };
-
-        let result: Response = self.client.post(&self.url("/auth"))
-            .json(&req).send().await?
-            .json().await?;
-
-        assert(result.code, "Auth")?;
-
-        Ok(Session {
-            connection: self.clone(),
-            key: result.session.unwrap()
-        })
-    }
-
-    pub async fn run_command(&self, auth_key: &str, command: &str, args: &[&str]) -> HttpResult<String> {
-        #[serde(rename_all = "camelCase")]
-        #[derive(Serialize)]
-        struct Request<'s> {
-            auth_key: &'s str,
-            name: &'s str,
-            args: &'s [&'s str],
-        }
-
-        let req = Request {
-            auth_key,
-            name: command,
-            args,
-        };
-
-        let text = self.client.post(&self.url("/command/send"))
-            .json(&req).send().await?
-            .text().await?;
-
-        Ok(text)
-    }
-}
-
-impl Session {
-    /// Connecting the base url from this connection and the given string.
-    /// Note that this function only simply connects two string, so you must ensure the given string starts with the '/' separator.
-    pub fn url(&self, path: &str) -> String {
-        self.connection.url(path)
-    }
-
-    /// Return the client of this session
-    pub fn client(&self) -> &Client {
-        &self.connection.client
-    }
-
-    /// Binding the session with the given QQ ID.
-    /// Note that one session can only bind with one QQ ID.
-    pub async fn verify(&self, qq: Target) -> HttpResult<()> {
-        #[derive(Serialize)]
-        struct Request {
-            #[serde(rename = "sessionKey")]
-            session_key: String,
-            qq: Target,
-        }
-
-        let req = Request {
-            session_key: self.key.clone(),
-            qq,
-        };
-
-        let result: CommonResponse = self.client().post(&self.url("/verify"))
-            .json(&req).send().await?
-            .json().await?;
-
-        assert(result.code, "Verify")
-    }
-
-    /// Release the QQ ID which this session bound before.
-    pub async fn release(&self, qq: Target) -> HttpResult<()> {
-        #[derive(Serialize)]
-        struct Request {
-            #[serde(rename = "sessionKey")]
-            session_key: String,
-            qq: Target,
-        }
-
-        let req = Request {
-            session_key: self.key.clone(),
-            qq,
-        };
-
-        let resp: CommonResponse = self.client().post(&self.url("/release"))
-            .json(&req).send().await?
-            .json().await?;
-
-        assert(resp.code, "Release")
     }
 }
